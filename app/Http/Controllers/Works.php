cdcce25f1366c4ba56223897060634c812416386
Chelsea <chelsealouis.graphiste@gmail.com>
2021-04-29 11:14:27 +0200

    Création et test de l'API LARAVEL

--- app/Http/Controllers/Works.php
+++ app/Http/Controllers/Works.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Work;
 
 class Works extends Controller
 {
-    //
+    public function index() {
+      return response()->json(Work::all());
+    }
 }
