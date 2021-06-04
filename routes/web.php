bab919ee43ce7168ff8703ff6100d1dd3d72600f
Chelsea <chelsealouis.graphiste@gmail.com>
2021-06-04 15:35:53 +0200

    Installation de voyager

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('templates/index');
 })->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
